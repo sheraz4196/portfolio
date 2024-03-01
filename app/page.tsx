75e91b614732f625269108d50ae42668085c5eac
sheraz-manzoor <sherazmanzoor54@gmail.com>
2024-03-01 16:28:27 +0500

    fix: made a checkbox component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Loading from "$/components/loading-indicator/Text-loader";
+import Checkbox from "$/components/Checkbox";
 
 export default function Home() {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-white">
-      <Loading />
+      <Checkbox label="True" isChecked={true} />
     </div>
   );
 }
