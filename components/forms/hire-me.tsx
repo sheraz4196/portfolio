99edc64f198e0ca1d5e343479f4df8f4e23861ef
sheraz4196 <sheraztoor321@gmail.com>
2024-06-03 21:46:42 +0500

    feat: completed hire me form

--- components/forms/hire-me.tsx
+++ components/forms/hire-me.tsx
@@ -1,6 +1,14 @@
 'use client';
 import { z } from 'zod';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Input } from '../ui/input';
@@ -12,6 +20,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+import { Checkbox } from '../ui/checkbox';
+import { RadioGroup, RadioGroupItem } from '../ui/radio-group';
 
 const hireMeFormSchema = z.object({
   name: z.string().min(3, { message: 'Please Enter Your Valid Name' }),
@@ -61,6 +71,14 @@ export default function HireMeForm() {
     '$10,000 - $20,000',
     'More than $20,000',
   ];
+  const RequiredFeatures = [
+    'Responsive Design',
+    'E-commerce Functionality',
+    'Contact Form',
+    'Blog',
+    'SEO Optimization',
+    'Other',
+  ];
   const form = useForm({
     resolver: zodResolver(hireMeFormSchema),
     defaultValues: {
@@ -81,6 +99,7 @@ export default function HireMeForm() {
   return (
     <Form {...form}>
       <form className="grid grid-cols-2 gap-4 p-4 lg:gap-8 lg:p-6">
+        <h2 className="col-span-2 lg:text-2xl">Enter Basic Info of your project</h2>
         <FormField
           control={form.control}
           name="name"
@@ -144,6 +163,33 @@ export default function HireMeForm() {
             </FormItem>
           )}
         />
+        <h3>Please select required features.</h3>
+        <FormField
+          control={form.control}
+          name="requiredFeatures"
+          render={({ field }) => (
+            <FormItem className="col-span-2">
+              <FormControl>
+                <div className="flex gap-6">
+                  {RequiredFeatures.map((feature, index) => (
+                    <div key={index} className="flex items-center gap-2">
+                      <Checkbox
+                        id={feature}
+                        onCheckedChange={(checked) => {
+                          return checked
+                            ? field.onChange([...field.value, feature])
+                            : field.onChange(field.value?.filter((value) => value !== feature));
+                        }}
+                      />
+                      <FormLabel htmlFor={feature}>{feature}</FormLabel>
+                    </div>
+                  ))}
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <FormField
           control={form.control}
           name="budgetRange"
@@ -174,6 +220,69 @@ export default function HireMeForm() {
             <FormItem>
               <FormControl>
                 <Input placeholder="Project Deadline" {...field} type="date" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="currentWebsite"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="URL (Optional)" {...field} />
+              </FormControl>
+              <FormDescription>Enter Url of your current website if you have.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <h2 className="col-span-2 lg:text-2xl">Additional Information</h2>
+        <FormField
+          control={form.control}
+          name="projectDescription"
+          render={({ field }) => (
+            <FormItem className="col-span-2">
+              <FormControl>
+                <Textarea
+                  placeholder="Additional Information You want to provide (Optional)"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <h3>Prefered Contact Method</h3>
+        <FormField
+          control={form.control}
+          name="currentWebsite"
+          render={({ field }) => (
+            <FormItem className="bg-red-700">
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex items-center gap-4"
+                >
+                  <FormItem>
+                    <FormControl>
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="Email" id="mail" />
+                        <FormLabel htmlFor="mail">Email</FormLabel>
+                      </div>
+                    </FormControl>
+                  </FormItem>
+                  <FormItem>
+                    <FormControl>
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="Phone" id="phone" />
+                        <FormLabel htmlFor="phone">Whatsapp</FormLabel>
+                      </div>
+                    </FormControl>
+                  </FormItem>
+                </RadioGroup>
               </FormControl>
               <FormMessage />
             </FormItem>
