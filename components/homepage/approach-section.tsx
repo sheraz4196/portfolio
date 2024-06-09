cfc922d443d68d39c177261eff9325700d40c9c9
sheraz4196 <sheraztoor321@gmail.com>
2024-06-10 01:56:33 +0500

    fix: minor layout issue

--- components/homepage/approach-section.tsx
+++ components/homepage/approach-section.tsx
@@ -1,8 +1,8 @@
 export default function ApproachSection() {
   return (
     <section className="flex items-center justify-center p-4 lg:p-6">
-      <div>
-        <h3 className="text-3xl">Approaches</h3>
+      <div className="w-full">
+        <h3 className="text-3xl lg:mb-20">Approaches</h3>
         <div className="grid grid-cols-3"></div>
       </div>
     </section>
