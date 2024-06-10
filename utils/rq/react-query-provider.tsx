0898cedf0739d43bdcd49857265cb6a9b8a550ec
sheraz4196 <sheraztoor321@gmail.com>
2024-06-10 23:45:33 +0500

    feat: initialized react query

---
+++ utils/rq/react-query-provider.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react';
+
+export function RqProvider(props: { children: React.ReactNode }) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+            retry: 1,
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      {props.children}
+      {<ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
+  );
+}
