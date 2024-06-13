76744334175f8e4fe4bf683df71e8cb9606fa617
sheraz4196 <sheraztoor321@gmail.com>
2024-06-14 02:22:14 +0500

    feat: improve toolkit hero

--- components/toolkit/toolkit-hero.tsx
+++ components/toolkit/toolkit-hero.tsx
@@ -3,14 +3,20 @@ import { Boxes } from '../ui/backgound-boxes';
 
 export default function ToolkitHero() {
   return (
-    <div className="relative flex h-96 w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-slate-900">
-      <div className="pointer-events-none absolute inset-0 z-20 h-full w-full bg-slate-900 [mask-image:radial-gradient(transparent,white)]" />
+    <div className="relative flex min-h-[90vh] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-zinc-900 p-4 lg:p-6">
+      <div className="pointer-events-none absolute inset-0 z-20 h-full  bg-zinc-900 [mask-image:radial-gradient(transparent,white)]" />
 
       <Boxes />
-      <h1 className={cn('relative z-20 text-xl text-white md:text-4xl')}>Tailwind is Awesome</h1>
-      <p className="relative z-20 mt-2 text-center text-neutral-300">
-        Framer motion is the best animation library ngl
-      </p>
+      <div className="flex max-w-4xl flex-col gap-6 text-center">
+        <h1 className={cn('relative z-20 text-xl text-white md:text-4xl')}>
+          Developer&apos;s Toolkit: Essential Tools and Libraries
+        </h1>
+        <p className="relative z-20 mt-2 text-center text-neutral-300">
+          Discover the essential tools and libraries that streamline my development process. From
+          powerful frameworks to handy utilities, this curated list covers everything you need to
+          enhance your coding efficiency and build impressive projects.
+        </p>
+      </div>
     </div>
   );
 }
