97664d180dfd350859c81ebcced3fbbca2bc28b9
sheraz4196 <sheraztoor321@gmail.com>
2024-06-15 18:15:39 +0500

    fix:  spacings

--- components/homepage/personal-gallery.tsx
+++ components/homepage/personal-gallery.tsx
@@ -3,10 +3,12 @@ import { DirectionAwareHover } from '../ui/direction-awaze-hover';
 
 export default function PersonalGallery() {
   return (
-    <section className="flex gap-6 p-4 lg:p-6">
-      {Aesthetics.map((picture, index) => (
-        <DirectionAwareHover imageUrl={picture} key={index} />
-      ))}
+    <section className="p-4 lg:p-6">
+      <article className="flex gap-6 pb-24 pt-12">
+        {Aesthetics.map((picture, index) => (
+          <DirectionAwareHover imageUrl={picture} key={index} />
+        ))}
+      </article>
     </section>
   );
 }
